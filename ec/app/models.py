503a39dc74113059f09d062a58f451a4f4bffe59
Renuka Devi G <renukagopaloo1@gmail.com>
2023-09-15 18:31:20 +0530

    Deploying to Railway

---
+++ ec/app/models.py
@@ -0,0 +1,127 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+STATE_CHOICES = (
+    ('Andaman & Nicobar Island', 'Andaman & Nicobar Island'),
+    ('Andhra Pradesh', 'Andhra Pradesh'),
+    ('Arunachal Pradesh', 'Arunachal Pradesh'),
+    ('Assam', 'Assam'),
+    ('Bihar', 'Bihar'),
+    ('Chandigarh', 'Chandigarh'),
+    ('Chattisgarh', 'Chattisgarh'),
+    ('Dadra & Nagar Haveli', 'Dadra & Nagar Haveli'),
+    ('Daman & Diu', 'Daman & Diu'),
+    ('Delhi', 'Delhi'),
+    ('Goa', 'Goa'),
+    ('Gujarat', 'Gujarat'),
+    ('Haryana', 'Haryana'),
+    ('Himachal Pradesh', 'HImachal Pradesh'),
+    ('Jammu & Kashmir', 'Jammu & Kashmir'),
+    ('Jharkhand', 'Jharkhand'),
+    ('Karnataka', 'Karnataka'),
+    ('Kerala', 'Kerala'),
+    ('Lakshadweep', 'Lakshadweep'),
+    ('Madhya Pradesh', 'Madhya Pradesh'),
+    ('Maharashtra', 'Maharsahtra'),
+    ('Manipur', 'Manipur'),
+    ('Meghalaya', 'Meghalaya'),
+    ('Mizoram', 'Mizoram'),
+    ('Nagaland', 'Nagaland'),
+    ('Odissa', 'Odissa'),
+    ('Puducherry', 'Puducherry'),
+    ('Punjab', 'Punjab'),
+    ('Rajasthan', 'Rajasthan'),
+    ('Sikkim', 'Sikkim'),
+    ('Tamil Nadu', 'Tamil Nadu'),
+    ('Telangana', 'Telangana'),
+    ('Tripura', 'Tripura'),
+    ('Uttarkhand', 'Uttarkhand'),
+    ('Uttar Pradesh', 'Uttar Pradesh'),
+    ('West Bengal', 'West Bengal'),
+    )
+
+
+CATEGORY_CHOICES=(
+    ('WC','WomensClothing'),
+    ('WA','WomensAccessories'),
+    ('SH','Shoes'),
+    
+    
+)
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    composition = models.TextField(default='')
+    prodapp = models.TextField(default='')
+    #brand = models.CharField(max_length=100)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to='product')
+    def _str_(self):
+        return self.title
+    
+class Customer(models.Model):
+    id = models.AutoField(primary_key=True)
+    #id = models.IntegerField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    mobile = models.IntegerField(default=0)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES,max_length=100) 
+    def __str__(self):
+        return self.name   
+    
+class Cart(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    
+    
+    @property
+    def total_cost(self):
+        return self.quantity * self.product.discounted_price
+    
+STATUS_CHOICES = (
+    ('Accepted','Accepted'),
+    ('Packed','Packed'),
+    ('On the way','On the way'),
+    ('Delivered','Delivered'),
+    ('Cancel','Cancel'),
+    ('Pending','Pending'),
+)
+    
+
+    
+class Payments(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    amount = models.FloatField()
+    razorpay_order_id = models.CharField(max_length=100,blank=True,null=True)
+    razorpay_payment_status = models.CharField(max_length=100,blank=True,null=True)
+    razorpay_payment_id = models.CharField(max_length=100,blank=True,null=True)
+    paid = models.BooleanField(default=False)
+    
+
+    
+class OrdersPlaced(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    ordered_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50,choices=STATUS_CHOICES,default='Pending')
+    payment = models.ForeignKey(Payments,on_delete=models.CASCADE,default="")
+    @property
+    def total_cost(self):
+        return self.quantity * self.product.discounted_price
+    
+    
+class Wishlist(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
